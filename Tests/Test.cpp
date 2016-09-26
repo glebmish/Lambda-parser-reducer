87e877f50756517e9c911bfffe27e957a36d1213
glebmish <bboygleb@gmail.com>
2016-09-26 23:00:55 +0300

    Added logging

--- Tests/Test.cpp
+++ Tests/Test.cpp
@@ -1,7 +1,9 @@
 #include "Test.h"
+#include "../Logger.cpp"
 using namespace std;
 
-bool check(istream &answer, istream &outputRead) {
+bool check(istream &answer, istream &outputRead, ostream &log) {
+    LOG(log);
     char ra_char, ca_char;
 
     while (!answer.eof() && !outputRead.eof()) {
@@ -17,18 +19,23 @@ bool check(istream &answer, istream &outputRead) {
         return false;
 }
 
-vector<string> get_tests_files(string tests_dir) {
+vector<string> get_tests_files(string tests_dir, ostream &log) {
+    LOG(log);
+
 	vector<string> inputFiles;
     
     DIR *dir;
 	struct dirent *ent;
 
 	dir = opendir(tests_dir.c_str());
+
+    LOG_MESSAGE(tests_dir + " opened");
+
 	while ((ent = readdir(dir)) != NULL) {
 		string tmp(ent->d_name);
 		if (tmp[0] == '.' || tmp[tmp.size() - 2] == '.') 
 			continue;
-
+        
 		inputFiles.push_back(tmp);
 	}
 
@@ -36,14 +43,24 @@ vector<string> get_tests_files(string tests_dir) {
     return inputFiles;
 }
 
+bool cmp(string a, string b) {
+    return atoi(a.c_str()) < atoi(b.c_str());
+}
 
-void test(ostream &log) {
-    string tests_dir = "./lambda_examples/";
-    vector<string> testList = get_tests_files(tests_dir);
+void test(ostream &result, ostream &log) {
+    LOG(log);
+    string tests_dir("./Tests/lambda_examples/");
+
+    vector<string> testList = get_tests_files(tests_dir, log);
+    sort(testList.begin(), testList.end(), cmp);
+    
+    LOG_MESSAGE("tests in " + tests_dir + ": " + log_array(&testList[0], testList.size()));
 
     for (unsigned i = 0; i < testList.size(); i++) {
+        LOG_MESSAGE("test " + testList[i]);
+
         string inputFile = tests_dir + testList[i];
-        string outputFile = inputFile + "tmp";
+        string outputFile = inputFile + ".tmp";
         string answerFile = inputFile + ".a";
 
         ifstream input(inputFile.c_str());
@@ -52,15 +69,24 @@ void test(ostream &log) {
 
         string exp;
         getline(input, exp);
+
+        LOG_MESSAGE("lambda: " + exp);
+
         calc(output, exp);
 
         output.close();
         ifstream outputRead(outputFile.c_str());
 
-        bool isEqual = check(answer, outputRead);
-        if (isEqual)
-            log << "Test " << i << " passed.\n";
-        else 
-            log << "Test " << i << " failed.\n";
+        bool isEqual = check(answer, outputRead, log);
+        if (isEqual) {
+            LOG_MESSAGE("Test " + testList[i] + " passed.\n");
+            result << "Test " << testList[i] << " passed.\n";
+        } else { 
+            LOG_MESSAGE("Test " + testList[i] + " failed.\n");
+            result << "Test " << testList[i] << " failed.\n";
+        }
+
+        outputRead.close();
+        remove(outputFile.c_str());
     }
 }
--- Tests/Test.h
+++ Tests/Test.h
@@ -3,7 +3,8 @@
 #include <dirent.h>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 #include "../Calculation/Calc.h"
 
-void test(ostream &log);
+void test(ostream &result, ostream &log = *(new ostream(0)));
