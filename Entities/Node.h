9d94fcea2f78383ac287e7f6db45b160557ee648
glebmish <bboygleb@gmail.com>
2016-09-28 16:13:39 +0300

    Deleted virtual constructor and getvalue function

--- Entities/Node.h
+++ Entities/Node.h
@@ -27,14 +27,10 @@ class Node {
     virtual void get_expression(ostream &out, string wrapEntity = "none", Position position = _right) = 0;
     virtual std::vector<string> get_tree_view(int shift = 0) = 0;
 
-    virtual string getvalue() = 0;
-
     virtual Node* reduce(Pool *pool) = 0;
     virtual bool isredex() = 0;
     virtual Node* substitute(Pool *pool, int free, int who, Node *with) = 0;
     virtual Node* changeprior(Pool *pool, int prior, map<int, int> m = map<int,int>()) = 0;
 
     virtual Node* copy(Pool *pool) = 0;
-
-    virtual ~Node();
 };
