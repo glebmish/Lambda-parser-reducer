5d8997f59b92117ceaac85dd63ed3711d02b1ac4
glebmish <bboygleb@gmail.com>
2016-09-30 22:06:56 +0300

    Changed Main.cpp and Makefile to suit CLI module

--- Main.cpp
+++ Main.cpp
@@ -1,53 +1,27 @@
 #include <string>
-#include <istream>
 #include <ostream>
-#include <fstream>
+#include <iostream>
 using namespace std;
 
-#include "Calculation/Calc.h"
-#include "Tests/FunctionalTest.h"
+#include "CLI/CLI.h"
 #include "./Logger/Logger.h"
 
 int main(int argc, char *argv[]) {
-    bool isTest = false;
-    bool isLog = false;
-    
-    for (int i = 1; i < argc; ++i) {
-        if (string(argv[i]) == "--test") {
-            isTest = true;
-        }
-
-        if (string(argv[i]) == "--log") {
-            isLog = true;
-        }
-
+    if (argc > 2) {
+        cout << "Error: incorrect arguments\n";
+        return -1;
     }
     
     ostream *log;
-    if (isLog) {
+    if (argc == 2 && string(argv[1]) == "--log")
         log = &cout;
-    } else {
+    else
         log = new ostream(0);
-    }
 
     LOG(*log);
-    LOG_MESSAGE("flag --log is set");
+    LOG_MESSAGE("logging enabled");
 
-    LOG_MESSAGE("flag --test is set");
-    if (isTest) {
-        test(cout, *log);
-        return 0;
-    }
-
-    istream &in = cin;
-    ostream &out = cout;
-
-    string s;
-    getline(in, s);
-
-    LOG_MESSAGE("Lambda " + s + " have been read");
-
-    calc(out, s);
+    cli(*log);
 
     return 0;
 }
--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 all:
-	g++ Main.cpp Entities/*.cpp Calculation/*.cpp MemoryManagement/*.cpp Parsing/*.cpp Tests/*.cpp Logger/*.cpp -o lbr.out
+	g++ Main.cpp Entities/*.cpp CLI/*.cpp MemoryManagement/*.cpp Parsing/*.cpp Logger/*.cpp -o lbr.out
 
